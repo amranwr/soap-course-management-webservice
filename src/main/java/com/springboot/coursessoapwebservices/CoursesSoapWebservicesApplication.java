6b3f6931278bb99569050b25acb35044dea1700d
amranwr <amrtop2000@gmail.com>
2021-11-12 16:01:34 +0200

    first commit

---
+++ src/main/java/com/springboot/coursessoapwebservices/CoursesSoapWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.coursessoapwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesSoapWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesSoapWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/coursessoapwebservices/CoursesSoapWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.coursessoapwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesSoapWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
